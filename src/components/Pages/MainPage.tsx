a3870ce19b8b564b2991455b4c3980c9935d325d
2008MishaPro <burdink2309@gmail.com>
2025-03-07 12:42:04 +0300

    add pagination

--- src/components/Pages/MainPage.tsx
+++ src/components/Pages/MainPage.tsx
@@ -3,29 +3,58 @@ import { GetUsers } from "../requests/userFeach";
 import { UserCard } from "../Elements/Cards/UserCard";
 
 export const MainPage = () => {
-  const [users, setUsers] = useState([
-    {
-      id: "",
-      name: "",
-      username: "",
-      email: "",
-      phone: "",
-      website: "",
-    },
-  ]);
+  const [users, setUsers] = useState([]);
+  const [currentPage, setPage] = useState(1);
+  const [canShowNext, setShowState] = useState(true);
+  const usersLimitPerPage = 12;
   useEffect(() => {
-    const getData = async () => {
-      const data = await GetUsers("https://jsonplaceholder.typicode.com/users");
+    const getData = async (page: number) => {
+      const data = await GetUsers(
+        `https://jsonplaceholder.typicode.com/users?_page=${page}&_limit=${usersLimitPerPage}`
+      );
+      if (data && data.length > 0) {
+        setShowState(true);
+      } else {
+        setShowState(false);
+      }
       setUsers(data);
     };
-    getData();
-  }, []);
+    getData(currentPage);
+  }, [currentPage]);
+
+  const UserRender = () => {
+    if (canShowNext) {
+      return (
+        <>
+          {users.map((user) => (
+            <UserCard {...user} key={user.id} />
+          ))}
+        </>
+      );
+    } else {
+      return <>Больше ничего нет 😭</>;
+    }
+  };
+  const nextButton = () => {
+    if (canShowNext) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+  const prevButton = () => {
+    setPage((prevPage) => (prevPage > 1 ? prevPage - 1 : 1));
+  };
   return (
     <div className="mainPage">
       <div className="usersContainer">
-        {users.map((user) => (
-          <UserCard {...user} key={user.id} />
-        ))}
+        <UserRender />
+      </div>
+      <div className="buttons">
+        <button onClick={prevButton} disabled={currentPage === 1}>
+          ←
+        </button>
+        <button onClick={nextButton} disabled={!canShowNext}>
+          →
+        </button>
       </div>
     </div>
   );
